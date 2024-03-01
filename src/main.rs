11987d9d550470fd63a2d2228787c442b83da595
pedropark99 <pedropark99@gmail.com>
2024-02-29 21:33:05 -0300

    Add changes

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use json;
 use json::{JsonValue, JsonError};
 
 
-pub struct FlowField {
+pub struct NoiseField {
     field:Array2D<f64>,
     height:u8,
     width:u8
 }
 
-impl FlowField {
-    pub fn new(seed:u8, field_width:u8, field_height:u8) -> FlowField {
+impl NoiseField {
+    pub fn new(seed:u8, field_width:u8, field_height:u8) -> NoiseField {
         let noise_gen = OpenSimplex::new(seed.into());
         let mut flow_field = Array2D::filled_with(
             0 as f64,
@@ -30,7 +30,7 @@ impl FlowField {
             }
         }
 
-        FlowField {
+        NoiseField {
             field: flow_field,
             height: field_height,
             width: field_width
@@ -38,8 +38,8 @@ impl FlowField {
     }
 
     pub fn get_angle(&self, x:f64, y:f64) -> f64 {
-        let x = FlowField::get_flow_field_col(x);
-        let y = FlowField::get_flow_field_row(y);
+        let x = NoiseField::get_flow_field_col(x);
+        let y = NoiseField::get_flow_field_row(y);
         self.field[(x as usize, y as usize)]
     }
 
@@ -60,35 +60,12 @@ impl FlowField {
 }
 
 
-pub fn get_cases() -> JsonValue {
-    let cases = r#"[
-      {},
-      {"x": [1.0, 1.5], "y": [0.5, 1.0]},
-      {"x": [1.5, 1.0], "y": [1.0, 1.5]},
-      {"x": [1.5, 1.0], "y": [0.5, 1.0]},
-      {"x": [1.0, 0.5], "y": [1.5, 1.0]},
-      {"x": [1.0, 1.5, 0.5, 1.0], "y": [1.0, 0.5, 1.5, 1.0]},
-      {"x": [1.0, 0.5], "y": [1.0, 1.5]},
-      {"x": [1.0, 0.5], "y": [0.5, 1.0]},
-      {"x": [0.5, 1.0], "y": [1.0, 0.5]},
-      {"x": [1.0, 1.5], "y": [1.0, 0.5]},
-      {"x": [0.5, 1.0, 1.0, 0.5], "y": [1.5, 1.0, 1.0, 1.5]},
-      {"x": [1.5, 1.0], "y": [1.0, 0.5]},
-      {"x": [0.5, 1.0], "y": [1.5, 1.0]},
-      {"x": [1.0, 1.5], "y": [1.5, 1.0]},
-      {"x": [0.5, 1.0], "y": [1.0, 1.5]},
-      {}
-    ]"#;
-    let as_dict = json::parse(cases).unwrap();
-    return as_dict
-}
 
-
-pub fn threshhold(grid:&FlowField, width:u32, height:u32) -> Array2D<u8> {
+pub fn threshold(noise_field:&NoiseField, width:u32, height:u32) -> Array2D<u8> {
     let mut grid = Array2D::filled_with(0 as u8, width as usize, height as usize);
     for x in 0..width as usize {
         for y in 0..height as usize {
-            grid[(x, y)] = (grid[(x, y)] > 0) as u8
+            grid[(x, y)] = (noise_field.field[(x, y)] > 0.0) as u8;
         }
     }
     return grid
@@ -99,10 +76,15 @@ pub fn threshhold(grid:&FlowField, width:u32, height:u32) -> Array2D<u8> {
 pub fn main() {
     let width = 100;
     let height = 100;
-    let flow_field = FlowField::new(50, 100, 100);
-    let binary_grid = threshhold(&flow_field, width, height);
+    let noise_field = NoiseField::new(50, width.into(), height.into());
+    let binary_grid = threshold(&noise_field, width.into(), height.into());
     let dx = 1;
     let dy = 1;
 
+    for x in 0..width as usize {
+        for y in 0..height as usize {
+            a
+        }
+    }
 
 }
